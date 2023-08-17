94c99ff6539977c4c8e7403ff24553bbcae916ca
c_u_l8er_true ucimecu inomauto <travis.burandt@gmail.com>
2023-08-16 20:46:09 -0500

    hostname

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,35 @@
-<script>
-	import Counter from './Counter.svelte';
+<script lang="ts">
+	import { onMount } from 'svelte';
+
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
+
+	let categories: any;
+
+	onMount(async() => {
+		let hostname = window.location.hostname
+		if (hostname === 'localhost') {
+			hostname = 'store.subvind.com'
+		}
+    const response = await fetch(`https://backend.subvind.com/categories/hostname/${hostname}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+
+    if (response.ok) {
+      categories = await response.json();
+
+      setTimeout(() => {
+        let elms = document.querySelectorAll('.tabs')
+        var instance = M.Tabs.init(elms, {});
+      }, 0)
+    } else {
+      const errorData = await response.json();
+      alert(errorData.error);
+    }
+	})
 </script>
 
 <svelte:head>
@@ -25,7 +53,7 @@
 		try editing <strong>src/routes/+page.svelte</strong>
 	</h2>
 
-	<Counter />
+	{JSON.stringify(categories)}
 </section>
 
 <style>
