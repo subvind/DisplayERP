7a86f11f4c7d4a528589439526f8016bee1a4c8b
c_u_l8er_true ucimecu inomauto <travis.burandt@gmail.com>
2023-11-15 01:40:44 -0600

    auth

---
+++ src/routes/auth/register/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  import { AuthAccountRegister } from "subvind-components"
+
+	let organization: any;
+
+  onMount(async () => {
+    let erpHostname = window.location.hostname
+    if (erpHostname === 'localhost') {
+      erpHostname = 'videos.subvind.com'
+    }
+    const response = await fetch(`https://api.subvind.com/organizations/erpHostname/${erpHostname}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+
+    if (response.ok) {
+      organization = await response.json();
+    } else {
+      const errorData = await response.json();
+      alert(errorData.error);
+    }
+  })
+</script>
+
+<div class="container">
+  <br />
+  <br />
+  <AuthAccountRegister organization={organization} />
+</div>
+
+<style>
+  .container {
+    margin: 0 auto;
+    background: #222;
+    padding: 1em;
+    padding-top: 0;
+    color: #ccc;
+    position: relative;
+    border: 3px solid #333;
+    border-top: 0px;
+    border-bottom: 0px;
+  }
+</style>
